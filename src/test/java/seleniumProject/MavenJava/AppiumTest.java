7465553bde985bf74267c26f91b7033ec222d33f
VikashKumar-s <vikash.k@utthunga.com>
2022-07-09 23:56:16 +0530

    09 jul

--- src/test/java/seleniumProject/MavenJava/AppiumTest.java
+++ src/test/java/seleniumProject/MavenJava/AppiumTest.java
@@ -30,4 +30,19 @@ public class AppiumTest {
 	{
 		System.out.println("Test2");
 	}
+	
+	public void Test3()
+	{
+		System.out.println("Test3");
+	}
+	
+	public void Test4()
+	{
+		System.out.println("Test4");
+	}
+	
+	public void Test5()
+	{
+		System.out.println("Test5");
+	}
 }
